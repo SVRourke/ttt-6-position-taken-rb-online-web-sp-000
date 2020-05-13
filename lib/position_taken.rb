6dcf38efc663cfc6b9d600b08d5b6cda756e144f
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-13 14:30:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,9 @@
 
 # method checks if spot is " " or ""
 def position_taken?(board, position)
-  if board[position] == " " or board[position] == ""
+  if board[position] == " " or 
+     board[position] == "" or
+     board[position] == nil
     false
   end 
   
